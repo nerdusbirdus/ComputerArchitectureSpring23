385845a13498cb7465e8c70edb43c86944cc4c86
Scrungus <galletasalada42069@gmail.com>
2023-04-15 19:53:12 -0700

    the joy of having written a working C program once

---
+++ NumArgs.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main (int numargs, char *argv[]) {
+   int n;
+n = numargs;
+   printf ("Number of arguments = %d which is cool\n", n);
+   return EXIT_SUCCESS;
+} 
