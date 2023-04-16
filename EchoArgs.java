ce65162d51c9f68ed7d6aa884148c20a6a5f1070
Scrungus <galletasalada42069@gmail.com>
2023-04-15 20:07:19 -0700

    made sure echoargs.java works

--- EchoArgs.java
+++ EchoArgs.java
@@ -1,10 +1,10 @@
 public class EchoArgs
-{ public static void main(String[] args)
+{ public static void main(String args[])
     { 
-        System.out.println("Number of command line args: " + args.length);
+        
             for(int i=0; i < args.length; i++) 
             {
-                
+                System.out.println("Command line args are: " + args[i]); 
             }
     }
 
