cb7b2afb47011d7f63d6e559961fab5ef977f9e8
nerdus mcbirdus <galletasalada42069@gmail.com>
2023-04-17 22:52:55 -0700

    adding stuffs

---
+++ myOwnIgnorance.java
@@ -0,0 +1,14 @@
+
+public class myOwnIgnorance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//This program displays my own ignorance of java programming
+	int aNumber;//this declares an integer called aNumber
+	aNumber= 5;//this assigns aNumber a value of 5
+	System.out.println("This program outputs the preset value of an integer");
+	//this prints text
+	System.out.println("That integer is: " + aNumber);
+	}
+
+}
