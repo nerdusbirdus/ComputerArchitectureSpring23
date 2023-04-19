8424d222211f60b13c6288edb8277614c20dfe78
nerdusbirdus <galletasalada42069@gmail.com>
2023-04-18 20:44:41 -0700

    yay

--- cprogs/addTwo.c
+++ cprogs/addTwo.c
@@ -1,9 +1,6 @@
-  UW PICO 5.09                                                                               File: addTwo.c                                                                                Modified  
-
 #include <stdio.h>
 #include <stdlib.h>
-int main( int n, char* args[]);
-{
+int main( int n, char* args[]); {
 int num1, num2;
 
 printf ("Number of command line arguments is %d\n", n);
@@ -16,8 +13,7 @@ num1 = atoi(args[1]);
 num2 = atoi(args[2]);
 
 printf ("The sum of %d and %d is %d\n", num1, num2, num1 + num2);
-return 0;
-}
+return 0; }
  
 
 
