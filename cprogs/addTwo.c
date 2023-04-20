990f680ec36c32514deef1fe435654406efc06e6
nerdusbirdus <galletasalada42069@gmail.com>
2023-04-20 15:07:48 -0700

    screwflanders

--- cprogs/addTwo.c
+++ cprogs/addTwo.c
@@ -1,38 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main( int n, char* args[]); {
-int num1, num2;
-
-printf ("Number of command line arguments is %d\n", n);
-
-if (n !=2) {
-printf ("usage: ./addTwo int int\n");
-return -1;}
- 
-num1 = atoi(args[1]);
-num2 = atoi(args[2]);
-
-printf ("The sum of %d and %d is %d\n", num1, num2, num1 + num2);
-return 0; }
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-^G Get Help                     ^O WriteOut                     ^R Read File                    ^Y Prev Pg                      ^K Cut Text                     ^C Cur Pos                      
-^X Exit                         ^J Justify                      ^W Where is                     ^V Next Pg                      ^U UnCut Text                   ^T To Spell                     
+// ./addTwo int int
+int main(int n, char* args[]) {
+  int num1, num2, max;
+  if (n < 3)  {
+    printf("Usage: ./addTwo int int\n");
+    return 1;
+    }
+  num1 = atoi(args[1]);
+  num2 = atoi(args[2]);
+  printf("addTwo of %d and %d is: %d\n", num1, num2, num1 + num2);
+  }
---
+++ cprogs/hailstone2.c
@@ -0,0 +1,21 @@
+#include <stdio.h> // need to include for user input
+
+int main() {
+    int n, length = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    while (n != 1) {
+        printf("%d, ", n);
+        length++;
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+    printf("1\n"); // print the last number in the sequence
+    length++; // count the last number in the sequence
+    printf("Length of the hailstone sequence: %d\n", length);
+    return 0;
+}
